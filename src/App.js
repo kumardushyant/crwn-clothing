7d3978db8effb261bfb57d0c10412a5749079942
Dushyant Nimbekar <dushyant.nimbekar@adidas.com>
2021-02-05 14:03:06 +0530

    Started adding routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import HomePage from './code/home/home';
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+)
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" >
+          <HomePage />
+        </Route>
+        <Route path="/shop/hats">
+          <HatsPage />
+        </Route>
+      </Switch>
     </div>
   );
 }
--- src/code/directory/directory.jsx
+++ src/code/directory/directory.jsx
@@ -1,51 +1,14 @@
 import React, { Component } from "react";
 import './directory.scss';
 import MenuItem from '../menu-item/menu-item';
+import SECTIONS_DATA from "./section.data";
 
 class Directory extends Component {
   constructor() {
     super();
 
     this.state = {
-      sections: [
-        {
-          title: "hats",
-          imageUrl:
-            "https://images.unsplash.com/photo-1575428652377-a2d80e2277fc?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=380&q=80",
-          id: 1,
-          linkUrl: "shop/hats",
-        },
-        {
-          title: "jackets",
-          imageUrl:
-            "https://images.unsplash.com/photo-1515614466515-e512e497a047?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=380&q=80",
-          id: 2,
-          linkUrl: "shop/jackets",
-        },
-        {
-          title: "sneakers",
-          imageUrl:
-            "https://images.unsplash.com/photo-1523212465813-857a9d1a19f4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=380&q=80",
-          id: 3,
-          linkUrl: "shop/sneakers",
-        },
-        {
-          title: "womens",
-          imageUrl:
-            "https://images.unsplash.com/photo-1568196004494-b1ee34f3b436?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=380&q=80",
-          size: "large",
-          id: 4,
-          linkUrl: "shop/womens",
-        },
-        {
-          title: "mens",
-          imageUrl:
-            "https://images.unsplash.com/photo-1550246140-29f40b909e5a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&w=380&q=80",
-          size: "large",
-          id: 5,
-          linkUrl: "shop/mens",
-        },
-      ],
+      sections: SECTIONS_DATA
     };
   }
 
@@ -53,8 +16,8 @@ class Directory extends Component {
       return (
           <div className="directory-menu">
               {
-                  this.state.sections.map(({ title, imageUrl, id, size}) => (
-                      <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+                  this.state.sections.map(({ id, ...otherSectionProps }) => (
+                      <MenuItem key={id} {...otherSectionProps} />
                   ))
               }
           </div>
