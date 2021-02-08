e84b3533f48c77e1dfcd4f4464670c5e9c56df6a
Dushyant Kumar <dushyant.nimbekar@adidas.com>
2021-02-08 14:59:40 +0530

    removed api keys

--- src/code/firebase/firebase.util.js
+++ src/code/firebase/firebase.util.js
@@ -2,14 +2,7 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-var firebaseConfig = {
-    apiKey: "AIzaSyDkswWNb9jHvDo7YyL64yp8Ykj1QwLgz0U",
-    authDomain: "tutorial-react-91fe5.firebaseapp.com",
-    projectId: "tutorial-react-91fe5",
-    storageBucket: "tutorial-react-91fe5.appspot.com",
-    messagingSenderId: "986164906144",
-    appId: "1:986164906144:web:cbde476fe0965facc161c5"
-};
+var firebaseConfig = // apply vault to get config;
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
